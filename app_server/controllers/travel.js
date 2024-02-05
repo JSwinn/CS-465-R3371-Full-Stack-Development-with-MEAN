7a7c79dc92d0377af570bd23744ad410606fa8d3
JSwinn <136933121+JSwinn@users.noreply.github.com>
2024-02-04 23:49:34 -0500

    Module 5 RESTful API Part 2

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,52 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
-/* GET travel view */
-const travel = (req, res) => {
-    const pageTitle = process.env.MY_TITLE + ' | Travel';
-    res.render('travel', { title: pageTitle, trips });
+// Render Travel List View
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.MY_TITLE + ' | Travel';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = "No trips exist in database!";
+        }
+    }
+    res.render('travel', {
+        title: pageTitle, 
+        trips: responseBody,
+        message
+    });
 };
 
+/* GET travel List View */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info(' >> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
+
+
 module.exports = {
-    travel
+    travelList
 };
