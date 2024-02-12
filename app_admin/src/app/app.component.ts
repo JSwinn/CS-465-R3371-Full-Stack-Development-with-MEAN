46d0d6a0950f87f7c52ee037eae77510f204a90a
JSwinn <136933121+JSwinn@users.noreply.github.com>
2024-02-12 13:01:17 -0500

    Module 6 part 3 (Trip card)

--- app_admin/src/app/app.component.ts
+++ app_admin/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TripListingComponent],
+  imports: [RouterOutlet, TripListingComponent, TripCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, PipeTransform } from '@angular/core';
 import { trips } from '../data/trips';
 import { CommonModule } from '@angular/common';
+import { TripCardComponent } from '../trip-card/trip-card.component';
 
 
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html'
 })
 
